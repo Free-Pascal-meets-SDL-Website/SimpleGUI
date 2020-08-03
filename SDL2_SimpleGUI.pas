26ac2e6840d973f3457d4a7c8455f19ed981b98c
Matthias <info@freepascal-meets-sdl.net>
2020-08-04 00:16:18 +0200

    new lib unit
    - replaces old lib unit
    - only to be called for simple gui

---
+++ SDL2_SimpleGUI.pas
@@ -0,0 +1,44 @@
+unit SDL2_SimpleGUI;
+{:< The Library Unit}
+
+{ Simple GUI is a generic SDL2/Pascal GUI library by Matthias J. Molski,
+  get more infos here:
+    https://github.com/Free-Pascal-meets-SDL-Website/SimpleGUI.
+
+  It is based upon LK GUI by Lachlan Kingsford for SDL 1.2/Free Pascal,
+  get it here: https://sourceforge.net/projects/lkgui.
+
+  Written permission to re-license the library has been granted to me by the
+  original author.
+
+  Copyright (c) 2016-2020 Matthias J. Molski
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to
+  deal in the Software without restriction, including without limitation the
+  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+  sell copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+  IN THE SOFTWARE. }
+
+interface
+
+uses
+  SDL2, SDL2_TTF,
+  SG_GuiLib_Base, SG_GuiLib_Element, SG_GuiLib_Canvas;
+
+implementation
+
+begin
+
+end.
